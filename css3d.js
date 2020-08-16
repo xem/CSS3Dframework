a3bc5e68230c1ee146aada972bab402e97511771
xem <maxime.euziere@gmail.com>
2020-08-16 11:12:08 +0200

    new version, with shading and lighter

--- css3d.js
+++ css3d.js
@@ -1,143 +1,132 @@
 ﻿C = {
-  unit: "px",
-  camX: 0,
-  camY: 0,
-  camZ: 0,
-  camRX: 0,
-  camRY: 0,
-  camRZ: 0,
-  sprite_count: 0,
-  sprites: [],
-  plane_count: 0,
-  cube_count: 0,
-  pyramid_count: 0,
-  options: {},
-  
-  $: id => document.getElementById(id),
-  
-  set_unit: u => {
-    C.unit = u;
-  },
-  
-  set_perspective: o => {
-    viewport.style.perspective = `${o}${C.unit}`;
-  },
-  
-  init: o => {
-    if(!o.css) o.css = "";
-    if(!o.html) o.html = "";
-    if(!o.g) o.g = "scene";
-    if(!o.o) o.o = "center";
-    if(o.o == "top left") o.x += o.w/2, o.y += o.h/2;
-    if(o.o == "top") o.y += o.h/2;
-    if(o.o == "top right") o.x -= o.w/2, o.y += o.h/2;
-    if(o.o == "right") o.x -= o.w/2;
-    if(o.o == "bottom right") o.x -= o.w/2, o.y -= o.h/2;
-    if(o.o == "bottom") o.y -= o.h/2;
-    if(o.o == "bottom left") o.x += o.w/2, o.y -= o.h/2;
-    if(o.o == "left") o.x += o.w/2;
-    if(!o.w) o.w = 0;
-    if(!o.h) o.h = 0;
-    if(!o.x) o.x = 0;
-    if(!o.y) o.y = 0;
-    if(!o.z) o.z = 0;
-    if(!o.rx) o.rx = 0;
-    if(!o.ry) o.ry = 0;
-    if(!o.rz) o.rz = 0;
-    if(!o.sx) o.sx = 1;
-    if(!o.sy) o.sy = 1;
-    if(!o.sz) o.sz = 1;
-    C.options[o.n] = o;
-  },
-  
-  group: o => {
-    if(!o.d && !(o.d === 0)) o.d = o.h;
-    C.init(o);
-    C.$(o.g).innerHTML += `<div id="${o.n}"class="group ${o.css}"style="position:absolute;width:${o.w}${C.unit};height:${o.d}${C.unit};transform:${C.tr(o)}">`;
-  },
-  
-  plane: o => {
-    if(!o.n) o.n = `plane${C.plane_count++}`;
-    C.init(o);
-    C.$(o.g).innerHTML += `<div id="${o.n}"class="plane ${o.css}"style="position:absolute;width:${o.w}${C.unit};height:${o.h}${C.unit};background:${o.b};transform-origin:${o.o};transform:${C.tr(o)}">${o.html}`;
-    C.camera();
-  },
-  
-  sprite: o => {
-    if(!o.n) o.n = `sprite${C.sprite_count++}`;
-    C.init(o);
-    C.$(o.g).innerHTML += `<div id="${o.n}"class="sprite ${o.css}"style="position:absolute;width:${o.w}${C.unit};height:${o.h}${C.unit};background:${o.b};transform-origin:${o.o};transform:${C.tr(o)}">${o.html}`;
-    C.sprites.push(o.n);
-    C.camera();
-  },
+unit: "px",
+camX: 0,
+camY: 0,
+camZ: 0,
+camRX: 0,
+camRY: 0,
+camRZ: 0,
+sprite_count: 0,
+sprites: [],
+plane_count: 0,
+cube_count: 0,
+pyramid_count: 0,
+options: {},
+$: t => self[t],
+set_unit: t => C.unit=t,
+set_perspective: t => viewport.style.perspective=`${t}${C.unit}`,
 
-  cube: o => {
-    if(!o.n) o.n = `cube${C.cube_count++}`;
-    C.init(o);
-    C.group(o);
-    C.plane({g:o.n,x:o.w/2,y:o.d/2,w:o.w,h:o.d,b:o.b,css:"bottom"}); // bottom
-    C.plane({g:o.n,y:o.d/2,w:o.d,h:o.h,b:o.b,rx:-90,ry:-90,o:"bottom",css:"left"}); // left
-    C.plane({g:o.n,x:o.w,y:o.d/2,w:o.d,h:o.h,b:o.b,rx:-90,ry:-90,o:"bottom",css:"right"}); // right
-    C.plane({g:o.n,x:o.w/2,y:0,w:o.w,h:o.h,b:o.b,rx:-90,o:"bottom",css:"back"}); // back
-    C.plane({g:o.n,x:o.w/2,y:o.d,w:o.w,h:o.h,b:o.b,rx:-90,o:"bottom",css:"front"}); // front
-    C.plane({g:o.n,x:o.w/2,y:o.d/2,z:o.h,w:o.w,h:o.d,b:o.b,css:"top"}); // top
-  },
+// Initialize an object's properties
+init: t => {
+  t.css||(t.css=""),
+  t.html||(t.html=""),
+  t.g||(t.g="scene"),
+  t.o||(t.o="center"),
+  t.o=="top left"&&(t.x+=t.w/2,t.y+=t.h/2),
+  t.o=="top"&&(t.y+=t.h/2),
+  t.o=="top right"&&(t.x-=t.w/2,t.y+=t.h/2),
+  t.o=="right"&&(t.x-=t.w/2),
+  t.o=="bottom right"&&(t.x-=t.w/2,t.y-=t.h/2),
+  t.o=="bottom"&&(t.y-=t.h/2),
+  t.o=="bottom left"&&(t.x+=t.w/2,t.y-=t.h/2),
+  t.o=="left"&&(t.x+=t.w/2),
+  t.w||(t.w=0),
+  t.h||(t.h=0),
+  t.x||(t.x=0),
+  t.y||(t.y=0),
+  t.z||(t.z=0),
+  t.rx||(t.rx=0),
+  t.ry||(t.ry=0),
+  t.rz||(t.rz=0),
+  t.sx||(t.sx=1),
+  t.sy||(t.sy=1),
+  t.sz||(t.sz=1),
+  C.options[t.n]=t
+},
 
-  pyramid: o => {
-    if(!o.n) o.n = `pyramid${C.pyramid_count++}`;
-    C.init(o);
-    C.group({n:o.n,g:o.g,x:o.x,y:o.y,z:o.z,w:100,d:100,rx:o.rx,ry:o.ry,rz:o.rz,sx:o.w/100,sy:o.d/100,sz:o.h/86.6025,css:o.css});
-    C.plane({g:o.n,x:50,y:50,w:100,h:100,b:o.b,css:"bottom"}); // bottom
-    C.plane({g:o.n,y:50,w:100,h:100,b:o.b,ry:-60,rz:90,css:"triangle left",o:"bottom"}); // left
-    C.plane({g:o.n,x:100,y:50,w:100,h:100,b:o.b,ry:-120,rz:90,css:"triangle right",o:"bottom"}); // right
-    C.plane({g:o.n,x:50,y:0,w:100,h:100,b:o.b,rx:-120,css:"triangle back",o:"bottom"}); // back
-    C.plane({g:o.n,x:50,y:100,w:100,h:100,b:o.b,rx:-60,css:"triangle front",o:"bottom"}); // front
-  },
+// Group of objects
+group: t => { 
+  t.d||t.d===0||(t.d=t.h),
+  C.init(t),
+  C.$(t.g).innerHTML+=`<div id="${t.n}"class="group ${t.css}"style="position:absolute;width:${t.w}${C.unit};height:${t.d}${C.unit};transform:${C.tr(t)}">`
+},
 
-  camera: o => {
-    if(o && (o.x || o.x === 0)) C.camX = o.x;
-    if(o && (o.y || o.y === 0)) C.camY = o.y;
-    if(o && (o.z || o.z === 0)) C.camZ = o.z;
-    if(o && (o.rx || o.rx === 0)) C.camRX = o.rx;
-    if(o && (o.ry || o.ry === 0)) C.camRY = o.ry;
-    if(o && (o.rz || o.rz === 0)) C.camRZ = o.rz;
-    C.camX += ((Math.random() - .5)/1000);
-    scene.style.transform = `translateX(${-C.camX}${C.unit})translateY(${-C.camY}${C.unit})translateZ(${-C.camZ}${C.unit})rotateX(${C.camRX}deg)rotateY(${C.camRY}deg)rotateZ(${C.camRZ}deg)`;
-    for(var i in C.sprites){
-      var s = C.$(C.sprites[i]);
-      var t = s.style.transform.replace(/ *rotate.*\(.*?deg\)/g,"");
-      s.style.transform = t + `rotateZ(${-C.camRZ}deg)rotateX(${-C.camRX}deg)`;
-    }
-  },
+// Plane
+plane: t => {
+  t.n||(t.n=`plane${C.plane_count++}`),
+  C.init(t),
+  C.$(t.g).innerHTML+=`<div id="${t.n}"class="plane ${t.css}"style="position:absolute;width:${t.w}${C.unit};height:${t.h}${C.unit};background:${t.b};transform-origin:${t.o};transform:${C.tr(t)}">${t.html}`,
+  C.camera()
+},
 
-  move: o => {
-    if(o.n){
-      var obj = C.$(o.n);
-      var opt = C.options[o.n];
-      if(o.x || o.x === 0) opt.x = o.x;
-      if(o.y || o.y === 0) opt.y = o.y;
-      if(o.z || o.z === 0) opt.z = o.z;
-      if(o.rx || o.rx === 0) opt.rx = o.rx;
-      if(o.ry || o.ry === 0) opt.ry = o.ry;
-      if(o.rz || o.rz === 0) opt.rz = o.rz;
-      C.options[o.n] = opt;
-      obj.style.transform = C.tr(opt);
-    }
-  },
-  
-  tr: o => `translateX(-50%)translateY(-50%)translateX(${o.x}${C.unit})translateY(${o.y}${C.unit})translateZ(${o.z}${C.unit})rotateX(${o.rx}deg)rotateY(${o.ry}deg)rotateZ(${o.rz}deg)scaleX(${o.sx})scaleY(${o.sy})scaleZ(${o.sz})`
+// Sprite (optional)
+sprite: t => {
+  t.n||(t.n=`sprite${C.sprite_count++}`),
+  C.init(t),
+  C.$(t.g).innerHTML+=`<div id="${t.n}"class="sprite ${t.css}"style="position:absolute;width:${t.w}${C.unit};height:${t.h}${C.unit};background:${t.b};transform-origin:${t.o};transform:${C.tr(t)}">${t.html}`,
+  C.sprites.push(t.n),
+  C.camera()
+},
+
+// Cube (optional)
+cube: t => {
+  t.n||(t.n=`cube${C.cube_count++}`),
+  C.init(t),
+  C.group(t),
+  C.plane({g:t.n,x:t.w/2,y:t.d/2,w:t.w,h:t.d,b:t.b,css:"bottom"}),
+  C.plane({g:t.n,y:t.d/2,w:t.d,h:t.h,b:t.b1||t.b,rx:-90,ry:-90,o:"bottom",css:"left"}),
+  C.plane({g:t.n,x:t.w,y:t.d/2,w:t.d,h:t.h,b:t.b2||t.b,rx:-90,ry:-90,o:"bottom",css:"right"}),
+  C.plane({g:t.n,x:t.w/2,y:t.d,w:t.w,h:t.h,b:t.b1||t.b,rx:-90,o:"bottom",css:"front"}),
+  C.plane({g:t.n,x:t.w/2,y:0,w:t.w,h:t.h,b:t.b2||t.b,rx:-90,o:"bottom",css:"back"}),
+  C.plane({g:t.n,x:t.w/2,y:t.d/2,z:t.h,w:t.w,h:t.d,b:t.b,css:"top"})
+},
+
+// Pyramid (optional)
+pyramid: t => {
+  t.n||(t.n=`pyramid${C.pyramid_count++}`),
+  C.init(t),
+  C.group({n:t.n,g:t.g,x:t.x,y:t.y,z:t.z,w:100,d:100,rx:t.rx,ry:t.ry,rz:t.rz,sx:t.w/100,sy:t.d/100,sz:t.h/86.6025,css:t.css}),
+  C.plane({g:t.n,x:50,y:50,w:100,h:100,b:t.b,css:"bottom"}),
+  C.plane({g:t.n,y:50,w:100,h:100,b:t.b,ry:-60,rz:90,css:"triangle left",o:"bottom"}),
+  C.plane({g:t.n,x:100,y:50,w:100,h:100,b:t.b2||t.b,ry:-120,rz:90,css:"triangle right",o:"bottom"}),
+  C.plane({g:t.n,x:50,y:0,w:100,h:100,b:t.b,rx:-120,css:"triangle back",o:"bottom"}),
+  C.plane({g:t.n,x:50,y:100,w:100,h:100,b:t.b1||t.b,rx:-60,css:"triangle front",o:"bottom"})
+},
+
+// Move the camera
+camera: t => {
+  t&&(t.x||0===t.x)&&(C.camX=t.x),
+  t&&(t.y||0===t.y)&&(C.camY=t.y),
+  t&&(t.z||0===t.z)&&(C.camZ=t.z),
+  t&&(t.rx||0===t.rx)&&(C.camRX=t.rx),
+  t&&(t.ry||0===t.ry)&&(C.camRY=t.ry),
+  t&&(t.rz||0===t.rz)&&(C.camRZ=t.rz),
+  C.camX+=(Math.random()-.5)/1e3,
+  scene.style.transform=`translateX(${-C.camX}${C.unit})translateY(${-C.camY}${C.unit})translateZ(${-C.camZ}${C.unit})rotateX(${C.camRX}deg)rotateY(${C.camRY}deg)rotateZ(${C.camRZ}deg)`;
+  for(var r in C.sprites){
+    var n=C.$(C.sprites[r]),
+    o=n.style.transform.replace(/ *rotate.*\(.*?deg\)/g,"");
+    n.style.transform=o+`rotateZ(${-C.camRZ}deg)rotateX(${-C.camRX}deg)`
+  }
+},
+
+// Move an object
+move: t => {
+  if(t.n){
+   var r=C.$(t.n),
+   n=C.options[t.n];
+   (t.x||0===t.x)&&(n.x=t.x),
+   (t.y||0===t.y)&&(n.y=t.y),
+   (t.z||0===t.z)&&(n.z=t.z),
+   (t.rx||0===t.rx)&&(n.rx=t.rx),
+   (t.ry||0===t.ry)&&(n.ry=t.ry),
+   (t.rz||0===t.rz)&&(n.rz=t.rz),
+   C.options[t.n]=n,
+   r.style.transform=C.tr(n)
+ }
+},
+
+// CSS3D transform string
+tr: t => `translateX(-50%)translateY(-50%)translateX(${t.x}${C.unit})translateY(${t.y}${C.unit})translateZ(${t.z}${C.unit})rotateX(${t.rx}deg)rotateY(${t.ry}deg)rotateZ(${t.rz}deg)scaleX(${t.sx})scaleY(${t.sy})scaleZ(${t.sz})`
+
 }
-
-/*
-C.plane({n:"ground",x:0,y:0,z:0,w:450,h:450,b:"#8D8"})
-C.camera({x:0,y:0,z:150})
-C.camera({rx:45,ry:0,rz:0})
-C.plane({n:"p1",w:100,h:100,x:0,y:0,z:0,rx:-90,b:"#def",o:"bottom"})
-C.plane({n:"t1",w:100,h:100,x:-150,y:-150,z:0,rx:-90,ry:30,b:"#edf",css:"triangle",o:"bottom"})
-C.sprite({n:"s1",w:100,h:200,x:150,y:-150,z:0,b:"#fed",o:"bottom"})
-C.sprite({n:"s2",w:100,h:100,x:0,y:-150,z:0,b:"#efd",css:"circle",o:"bottom"})
-C.cube({n:"c1",g:"g1",x:120,y:120,w:120,h:75,d:100,rz:45,b:"#ddd"})
-C.pyramid({n:"py1",g:"g1",x:-120,y:120,w:110,h:150,d:100,rz:-45,b:"#777"})
-C.move({n:"p1",x:0,y:10,z:250,rx:0,rz:0});
-C.move({n:"c1",z:50,ry:45});
-*/
